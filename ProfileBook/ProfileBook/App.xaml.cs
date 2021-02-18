c615338c8c1f725986dc6b66b719e03075754837
Evgeniy Seletkov <evgeniy.seletkov@gmail.com>
2021-02-18 07:29:38 +0200

    Добавьте файлы проекта.

---
+++ ProfileBook/ProfileBook/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ProfileBook.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProfileBook
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainListPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
